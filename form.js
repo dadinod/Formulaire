5e86a51ffaf069342a918867d838e73082d0ed53
dadinod <dadinod12@gmail.com>
2021-07-16 10:06:29 +0200

    correction html + fonction Indentité

---
+++ form.js
@@ -0,0 +1,12 @@
+
+(function(a,b){
+    var ident = document.getElementById("ident");
+    var compte = document.getElementById("compte");
+    var a = ident.value.length;
+    var b = 25;
+    
+    compte.value = [a,'/',b];
+    ident.maxLength = 25;
+})();
+
+
